8e9efdb33743eb3ed9cf2c6ae05445e05906f534
Abhisha Acharya <abhishaacharya0@gmail.com>
2024-07-14 11:25:03 +0545

    minor changes

--- components/Notice.vue
+++ components/Notice.vue
@@ -1,25 +1,29 @@
 <template>
-    <section>
-        <v-container>
-
-            <table>
-
-                <tr>
-                    <th>sn</th>
-                    <th>subject</th>
-
-                    <th>noticecategory</th>
-                    <th>file</th>
-
-                </tr>
-            </table>
-        </v-container>
-
-    </section>
+  <section class="py-10">
+    <v-container class="flex flex-col gap-4 border-l border-gray-400">
+      <h1 class="text-3xl text-[#447C9D] capitalize">notice(campus notice)</h1>
+      <div class="flex flex-col gap-4">
+        <div v-for="notice in noticeStore.notices" :key="notice.id">
+          <div>
+            {{ notice.subject }}
+          </div>
+          <div class="text-xs">
+            {{ notice.createdAt }}
+          </div>
+        </div>
+        <div class="border-b"></div>
+      </div>
+      <div class="text-right">
+        <nuxt-link to="/notices">
+          <v-btn color="#447C9D"> more  <v-icon icon="mdi-greater-than" /> <v-icon icon="mdi-greater-than" /></v-btn>
+        </nuxt-link>
+      </div>
+    </v-container>
+  </section>
 </template>
 
 <script setup>
 import { useNoticeStore } from "~/store/notice";
-const noticeStore =useNoticeStore();
-await noticeStore.getNotices();
+const noticeStore = useNoticeStore();
+
 </script>
