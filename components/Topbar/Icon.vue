3f9b6a44c9346c5c1cdf982bb18d459ce4498f30
Abhisha Acharya <abhishaacharya0@gmail.com>
2024-07-14 13:39:14 +0545

    minor changes

--- components/Topbar/Icon.vue
+++ components/Topbar/Icon.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="flex gap-2 ">
-        <v-icon icon="mdi-facebook" color="white"/>
+        <a :href="profileStore.school.facebook" target="_blank">
+    <v-icon icon="mdi-facebook" variant="flat" color="white"></v-icon>
+ </a>
+      
         <v-icon icon="mdi-youtube" color="white"/>
-        <v-icon icon="mdi-linkedin" color="white"/>
+        <v-icon icon="mdi-linkedin" color="white"/> 
         <div class=" border-r">  
 
         </div>
     </div>
 </template>
+<script setup>
+import { useProfileStore } from '~/store/profile';
+const profileStore= useProfileStore();
+</script>
 
 
